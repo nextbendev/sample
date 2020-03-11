fa19386266aacd26103a711b928e0fd87993d3c2
Ben S <bendev@Bens-MacBook-Air.local>
2020-03-11 12:06:43 -0400

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root 'static_pages#home'
   get '/home', to: 'static_pages#home'
   get '/show', to: 'static_pages#show'
   get '/help', to: 'static_pages#help'
+  get  '/signup',  to: 'users#new'
 
 end
