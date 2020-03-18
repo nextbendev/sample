67e41d03a69ec234213f7ed3ab52cd91d549ad6b
Ben S <bendev@Bens-MacBook-Air.local>
2020-03-18 08:50:25 -0400

    channel delete

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,7 +5,7 @@ import 'popper.js/dist/esm/popper'
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
-require("channels")
+
 require("bootstrap/dist/js/bootstrap")
 
 
