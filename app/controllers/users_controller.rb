e7f71b6ab7bd7148c5b3ee9a146c8ead9afc504c
Ben S <bendev@Bens-MacBook-Air.local>
2020-03-18 08:16:40 -0400

    adv login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
-
   def show
-    @user = User.find(params[:id])
+      @user = User.find(params[:id])
   end
 
   def new
@@ -12,18 +11,31 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      # Handle a successful save.
-      flash[:success] = "You're in!"
+      flash[:success] = "Welcome to my Sample App!"
       redirect_to @user
     else
       render 'new'
     end
   end
 
-  private
+  def edit
+    @user = User.find(params[:id])
+  end
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
+  def update
+      @user = User.find(params[:id])
+      if @user.update_attributes(user_params)
+        flash[:success] = "Profile updated"
+        redirect_to @user
+      else
+        render 'edit'
+      end
+  end
+
+    private
+
+      def user_params
+        params.require(:user).permit(:name, :email, :password,
+                                     :password_confirmation)
+      end
 end
